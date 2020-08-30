2974605d8a6154669e5b2e6a26b2fb129043b9ee
AbnocChinwads <rnemo@hotmail.co.uk>
2020-08-30 20:32:09 +0100

    Initial commit

---
+++ static/darkMode.js
@@ -0,0 +1,29 @@
+var darkMode = localStorage.getItem(
+  "darkmode"
+); /* Sets localStorage value for the dark mode switch */
+
+if (darkMode) {
+  /* Sets classes for darker colours, and changes text colour on the darker backgrounds */
+  $("#darkModeSwitch").prop("checked", true);
+  $("body").toggleClass("body-dark");
+  $("nav").toggleClass("navbar-dark bg-dark");
+  $("footer").toggleClass("footer-dark");
+  $("#footer").children().toggleClass("anchor-styling-dark");
+}
+
+$("#darkModeSwitch").change(function () {
+  darkMode = !darkMode;
+  /* 
+        Switches the classes on and shows the switch as checked
+        if darkMode is read from the localStorage function,
+        and unchecks the switch and removes the classes if
+        darkMode is removed
+    */
+  if (darkMode) localStorage.setItem("darkmode", "1");
+  else localStorage.removeItem("darkmode");
+
+  $("body").toggleClass("body-dark", darkMode);
+  $("nav").toggleClass("navbar-dark bg-dark", darkMode);
+  $("footer").toggleClass("footer-dark", darkMode);
+  $("#footer").children().toggleClass("anchor-styling-dark", darkMode);
+});
