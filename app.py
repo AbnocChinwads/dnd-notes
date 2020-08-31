d9dc51c4a7ec9cce0c683aa5499ea5b7abc95644
AbnocChinwads <rnemo@hotmail.co.uk>
2020-08-31 10:49:28 +0100

    Adjusted user definition to GET from form

--- app.py
+++ app.py
@@ -1,22 +1,35 @@
 import os
+from os import path
 from flask import (Flask, flash, render_template,
                    redirect, request, url_for, Blueprint)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 mod = Blueprint("games", __name__)
 
+app.config["MONGO_DBNAME"] = "dnd-project"
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
-@app.route("/", methods=["GET", "POST"])
+mongo = PyMongo(app)
+
+
+@app.route("/")
 def index():
-    users = request.form.get("user")
-    if users == "user":
-        return redirect("user")
-    return render_template("index.html", user=users)
+    users = mongo.db.users.find()
+    return render_template("index.html", users=users)
 
 
-@app.route("/user")
-def user():
-    return render_template("user.html")
+@app.route("/user", methods=["GET"])
+def user(users_id):
+    users = mongo.db.users.find_one({
+                                     "_id": ObjectId(users_id)
+                                     })
+    user = users.username
+    return render_template("user.html", user=user)
 
 
 if __name__ == "__main__":
